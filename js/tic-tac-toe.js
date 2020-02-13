bfc7d0e6354ba763bcd73fb903dc1a5e7bfa24a2
Vanya Funez <VFunez@ucvts.tec.nj.us>
2020-02-13 12:33:10 -0500

    hmtl, css, js

---
+++ js/tic-tac-toe.js
@@ -0,0 +1,96 @@
+///////////////////// CONSTANTS /////////////////////////////////////
+const winningConditions = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+];
+///////////////////// APP STATE (VARIABLES) /////////////////////////
+let board;
+let turn;
+let win;
+let xWin = 0;
+let tie = 0;
+let oWin = 0;
+///////////////////// CACHED ELEMENT REFERENCES /////////////////////
+const squares = Array.from(document.querySelectorAll("#board div"));
+const message = document.querySelector("h2");
+///////////////////// EVENT LISTENERS ///////////////////////////////
+window.onload = init;
+document.getElementById("board").onclick = takeTurn;
+document.getElementById("reset-button").onclick = init;
+document.getElementById("x-button").onclick = firstX;
+document.getElementById("o-button").onclick = firstO;
+///////////////////// FUNCTIONS /////////////////////////////////////
+function init() {
+  board = [
+    "", "", "",
+    "", "", "",
+    "", "", ""
+  ];
+  turn = "X";
+  win = null;
+
+  render();
+}
+function firstX() {
+  document.getElementById('switch').innerHTML = "Turn: X";
+  turn = "X";
+}
+function firstO() {
+  document.getElementById('switch').innerHTML = "Turn: O";
+  turn = "O";
+}
+function render() {
+  board.forEach(function(mark, index) {
+    squares[index].textContent = mark;
+  });
+
+  message.textContent =
+    win === "T" ? "It's a tie!" : win ? `${win} wins!` : `Turn: ${turn}`;
+}
+function takeTurn(e) {
+  if (!win) {
+    let index = squares.findIndex(function(square) {
+      return square === e.target;
+    });
+
+    if (board[index] === "") {
+      board[index] = turn;
+      turn = turn === "X" ? "O" : "X";
+      win = getWinner();
+
+      render();
+    }
+    if (win === "T") {
+    tie++;
+    document.getElementById('tie-score').innerHTML = tie;
+    }
+  }
+}
+function getWinner() {
+  let winner = null;
+
+  winningConditions.forEach(function(condition, index) {
+    if (
+      board[condition[0]] &&
+      board[condition[0]] === board[condition[1]] &&
+      board[condition[1]] === board[condition[2]]
+    ) {
+      winner = board[condition[0]];
+    }
+  });
+
+    if (winner === "X") {
+      xWin++;
+      document.getElementById('x-score').innerHTML = xWin;
+    } else if (winner === "O") {
+      oWin++;
+      document.getElementById('o-score').innerHTML = oWin;
+    }
+  return winner ? winner : board.includes("") ? null : "T";
+}
